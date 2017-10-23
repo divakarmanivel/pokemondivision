98ef882318838dd2fd83f091443e85048da0acc0
Divakar Manivel <divakarmanivel@users.noreply.github.com>
2017-10-23 13:36:31 +0530

    Update requestAnimationFrame.js

--- js/requestAnimationFrame.js
+++ js/requestAnimationFrame.js
@@ -15,7 +15,7 @@
 
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
+            var currTime = performance.now();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
             var id = window.setTimeout(function () {
                     callback(currTime + timeToCall);
